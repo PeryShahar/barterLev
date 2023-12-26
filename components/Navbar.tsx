bcddb6499863fda887cdb6fd5400b6d7fdaeed12
PeryShahar <pery1shahar@gmail.com>
2023-12-26 13:44:59 +0200

    feat: add dialog to edit profile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,6 +18,17 @@ import {
     DropdownMenuSubTrigger,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
 import { Button } from './ui/button';
 
 const Navbar = () => {
@@ -37,22 +48,44 @@ const Navbar = () => {
                             <div style={{ zIndex: 10000 }} className='flex flex-row items-center gap-5'>
                                 <div className='flex flex-col items-center'>
                                 </div>
-
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <Image className='border-2 rounded-full' src={session?.user?.image!} alt="User Avatar" width={50} height={50} />
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent className="w-56">
-                                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                                        <DropdownMenuSeparator />
-                                        <DropdownMenuItem>
-                                            Edit Profile
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem onClick={() => signOut()}>
-                                            Log out
-                                        </DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
+                                <Dialog>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Image className='border-2 rounded-full' src={session?.user?.image!} alt="User Avatar" width={50} height={50} />
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent className="w-32 font-single">
+                                            <DropdownMenuLabel className='text-lg'>My Account</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DialogTrigger asChild>
+                                                <DropdownMenuItem className='text-base'>
+                                                    Edit Profile
+                                                </DropdownMenuItem>
+                                            </DialogTrigger>
+                                            <DropdownMenuItem className='text-base' onClick={() => signOut()}>
+                                                Log out
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                    <DialogContent className='font-single'>
+                                        <DialogHeader >
+                                            <DialogTitle>Edit profile</DialogTitle>
+                                            <DialogDescription className='text-xl'>
+                                                Make changes to your profile here. Click save when you're done.
+                                            </DialogDescription>
+                                        </DialogHeader>
+                                        <div className="grid w-full gap-1.5">
+                                            <Label className='text-black text-lg' htmlFor="give">What do you want to give?</Label>
+                                            <Textarea maxLength={200} className='text-black' placeholder="Type your message here." id="give" />
+                                        </div>
+                                        <div className="grid w-full gap-1.5">
+                                            <Label className='text-black text-lg' htmlFor="receive">What do you want to receive?</Label>
+                                            <Textarea maxLength={200} className='text-black' placeholder="Type your message here." id="receive" />
+                                        </div>
+                                        <DialogFooter>
+                                            <Button type="submit">Save changes</Button>
+                                        </DialogFooter>
+                                    </DialogContent>
+                                </Dialog>
                             </div>}
                     </div>
                 </div>
