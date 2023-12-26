1195a91f85d38a27dea3cbb5606b12223efefb88
PeryShahar <pery1shahar@gmail.com>
2023-12-26 13:20:13 +0200

    feat: add dropdown for editing profile / log in when click on user avatar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,21 @@ import React from 'react';
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import barterBuddyLogo from '@/public/barter-buddy-logo.png'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from './ui/button';
 
 const Navbar = () => {
     const { data: session, status } = useSession()
@@ -19,11 +34,25 @@ const Navbar = () => {
                     </div>
                     <div className="flex space-x-4">
                         {session &&
-                            <div className='flex flex-row items-center gap-5'>
+                            <div style={{ zIndex: 10000 }} className='flex flex-row items-center gap-5'>
                                 <div className='flex flex-col items-center'>
-                                    <Image className='border-2 rounded-full' src={session?.user?.image!} alt="User Avatar" width={50} height={50} />
                                 </div>
-                                <button className='text-white' onClick={() => signOut()}>Log out</button>
+
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Image className='border-2 rounded-full' src={session?.user?.image!} alt="User Avatar" width={50} height={50} />
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent className="w-56">
+                                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                                        <DropdownMenuSeparator />
+                                        <DropdownMenuItem>
+                                            Edit Profile
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem onClick={() => signOut()}>
+                                            Log out
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
                             </div>}
                     </div>
                 </div>
