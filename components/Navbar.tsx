98457eccd6784d0f16b0edfd34ac5d78666d22bf
PeryShahar <pery1shahar@gmail.com>
2023-12-27 10:04:31 +0200

    style: change bg color to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
 
     return (
-        <nav className="bg-blue-500 p-4">
+        <nav className=" p-4">
             <div className="container mx-auto">
                 <div className="flex items-center justify-between">
                     <div className='flex flex-row '>
