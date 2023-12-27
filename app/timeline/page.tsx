45c375d1da27bc032986f12e0a4fc8a6101842dd
PeryShahar <pery1shahar@gmail.com>
2023-12-27 09:38:14 +0200

    feat: add users cart

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,8 +1,15 @@
-import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-// import Tweet from "@/components/tweet";
+import { getServerSession } from "next-auth";
 import { authConfig, loginIsRequiredServer } from "@/lib/auth";
 import prisma from "@/lib/prisma";
+
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator"
 
 const wait = (ms: number) => new Promise((rs) => setTimeout(rs, ms));
 
@@ -14,24 +21,36 @@ export default async function Page() {
     const users = await prisma.users.findMany({
         select: { name: true, email: true, image: true }
     });
-    console.log('users: ', users);
 
     await wait(1000);
 
     return (
-        <>
-            {/*{session?.user?.image && <img src={session?.user?.image} alt="" />}*/}
-            {/* <h3>This is your timeline: {session?.user?.email}</h3> */}
+        <div className="border-3 border-black h-full">
             {users.map((user) => {
                 return (
-                    <>
-                        {user.name}
-                    </>
+                    <div className="p-4 flex gap-6 border-2 border-red-800 m-16 text-black rounded-2xl">
+                        <Avatar className="ml-2 self-center size-20">
+                            <AvatarImage src={user.image ?? ''} alt="User Avatar" />
+                            <AvatarFallback>X</AvatarFallback>
+                        </Avatar>
+                        <div>
+                            <h3 className="text-xl font-semibold">{user.name}</h3>
+                            <div className="flex flex-col gap-2 mt-2">
+                                <div><h6 className="font-medium text-lg">What do I want to give?</h6>
+                                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi et quidem quas numquam quaerat consequatur voluptate velit facere fugiat iure.</p>
+                                </div>
+                                <div>
+                                    <h6 className="font-medium text-lg">What do I want to receive?</h6>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas hic iure deleniti inventore debitis accusamus velit veritatis unde nemo reiciendis!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <Separator orientation="vertical" className="h-auto" />
+                        <Button className="self-center">Let's Talk!</Button>
+                    </div>
                 )
             })}
-            {/*{tweets.map((tweet: any, idx: number) => (*/}
-            {/*    <Tweet key={idx} tweet={tweet} />*/}
-            {/*))}*/}
-        </>
+
+        </div>
     );
 }
