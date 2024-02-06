e00404fae847cf0ca0a1f6e6adc574d4827286b1
PeryShahar <pery1shahar@gmail.com>
2024-02-06 15:54:33 +0200

    fix: add type to session

--- lib/auth.ts
+++ lib/auth.ts
@@ -64,10 +64,10 @@ export async function loginIsRequiredServer() {
     if (!session) return redirect("/");
 }
 
-export function loginIsRequiredClient() {
-    if (typeof window !== "undefined") {
-        const session = useSession();
-        const router = useRouter();
-        if (!session) router.push("/");
-    }
-}
+// export function loginIsRequiredClient() {
+//     if (typeof window !== "undefined") {
+//         const session = useSession();
+//         const router = useRouter();
+//         if (!session) router.push("/");
+//     }
+// }
