ca6a92e4e763b6e63209ade7726779b7d2440b5c
PeryShahar <pery1shahar@gmail.com>
2024-02-08 17:00:32 +0200

    feat: navigate to homepage by click the logo in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { useSession } from "next-auth/react";
+import { useRouter } from 'next/navigation'
 import Image from 'next/image';
 import Dropdown from './dropdown';
 
@@ -9,12 +10,13 @@ import barterBuddyLogo from '@/public/barter-logo.png'
 
 const Navbar = () => {
     const { data: session } = useSession()
+    const router = useRouter()
 
     return (
         <nav className="p-4">
             <div className="container mx-auto">
                 <div className={`flex items-center ${session ? "justify-between" : "justify-between max-md:justify-center"}`}>
-                    <div className='flex flex-row items-center gap-4'>
+                    <div onClick={() => router.push(session ? '/timeline' : '/')} className='flex flex-row items-center gap-4 cursor-pointer'>
                         <Image src={barterBuddyLogo} alt="BarterLev Logo" width={40} height={40} />
                         <div className='font-single text-white text-4xl tracking-widest'>BarterLev</div>
                     </div>
