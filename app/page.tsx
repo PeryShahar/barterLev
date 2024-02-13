9c62a479057ecd905ffbaf900dbac0aaf68a5241
PeryShahar <pery1shahar@gmail.com>
2024-02-13 17:37:45 +0200

    fix: src for how it works images

--- app/page.tsx
+++ app/page.tsx
@@ -84,20 +84,20 @@ export default async function SignInPage() {
           <div className="container mx-auto px-4">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
               <div className="flex flex-col items-center">
-                <Image className="zoom" src={'/sign-up.png'} width={250} height={250} alt="sign-up" />
+                <Image className="zoom" src={'/assets/images/sign-up.png'} width={250} height={250} alt="sign-up" />
                 <h3 className="text-xl text-blue-700 font-bold">Create an Account</h3>
                 <p className="text-center mt-2 text-lg">Sign up for a free account on our platform.</p>
               </div>
 
 
               <div className="flex flex-col items-center">
-                <Image className="zoom" src={'/magnifying-glasses.png'} width={250} height={250} alt="magnifying-glasses" />
+                <Image className="zoom" src={'/assets/images/magnifying-glasses.png'} width={250} height={250} alt="magnifying-glasses" />
                 <h3 className="text-xl text-blue-700 font-bold">Browse Listings</h3>
                 <p className="text-center mt-2 text-lg">Explore a wide range of listings from users all over the world.</p>
               </div>
 
               <div className="flex flex-col items-center">
-                <Image className="zoom" src={'/shaking-hands.png'} width={250} height={250} alt="shaking-hands" />
+                <Image className="zoom" src={'/assets/images/shaking-hands.png'} width={250} height={250} alt="shaking-hands" />
                 <h3 className="text-xl text-blue-700 font-bold">Make an Exchange</h3>
                 <p className="text-center mt-2 text-lg">Initiate an exchange with another user and negotiate terms.</p>
               </div>
