ca7224ec455aa89f0e1d949688ad5c6cb4e6ee86
PeryShahar <pery1shahar@gmail.com>
2024-02-13 16:44:37 +0200

    fix: static src of logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,6 @@ import { useRouter } from 'next/navigation'
 import Image from 'next/image';
 import Dropdown from './dropdown';
 
-import barterBuddyLogo from '@/public/assets/images/barter-logo.png'
 
 const Navbar = () => {
     const { data: session } = useSession()
@@ -17,7 +16,7 @@ const Navbar = () => {
             <div className="container mx-auto">
                 <div className={`flex items-center ${session ? "justify-between" : "justify-between max-md:justify-center"} max-[450px]:flex-col`}>
                     <div onClick={() => router.push(session ? '/timeline' : '/')} className='flex flex-row items-center gap-4 cursor-pointer'>
-                        <Image src={barterBuddyLogo} alt="BarterLev Logo" width={40} height={40} />
+                        <Image src={'/assets/images/barter-logo.png'} alt="BarterLev Logo" width={40} height={40} />
                         <div className='font-single text-white text-4xl tracking-widest'>BarterLev</div>
                     </div>
                     <div className="flex space-x-4">
