e5caa99073e69f716bcc2245a95b15f5580956fd
PeryShahar <pery1shahar@gmail.com>
2024-02-13 14:43:06 +0200

    fix: align navbar in small screen by using flex col

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     return (
         <nav className="p-4">
             <div className="container mx-auto">
-                <div className={`flex items-center ${session ? "justify-between" : "justify-between max-md:justify-center"}`}>
+                <div className={`flex items-center ${session ? "justify-between" : "justify-between max-md:justify-center"} max-[450px]:flex-col`}>
                     <div onClick={() => router.push(session ? '/timeline' : '/')} className='flex flex-row items-center gap-4 cursor-pointer'>
                         <Image src={barterBuddyLogo} alt="BarterLev Logo" width={40} height={40} />
                         <div className='font-single text-white text-4xl tracking-widest'>BarterLev</div>
