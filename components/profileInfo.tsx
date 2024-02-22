f26c255e11c22e0c47a0eb9fff53278ad4354346
PeryShahar <pery1shahar@gmail.com>
2024-02-22 14:25:31 +0200

    style: font, margin and add separator in profile info

--- components/profileInfo.tsx
+++ components/profileInfo.tsx
@@ -13,30 +13,31 @@ const ProfileInfo = ({ user, isMyProfile }: any) => {
     return (
         <div className="font-single flex flex-col m-8 p-6 text-black border-rose-400 border-2 rounded-lg shadow-2xl">
             <div className="flex flex-col items-center">
-                <Avatar className="ml-2 self-center size-40">
+                <Avatar className="ml-2 self-center size-52">
                     <AvatarImage src={user?.image ?? ''} alt="User Avatar" />
                     <AvatarFallback>X</AvatarFallback>
                 </Avatar>
-                <h2 className="text-3xl">{user?.name}</h2>
+                <h2 className="text-5xl">{user?.name}</h2>
                 <div className="flex items-center">
                     <IoLocationSharp />
-                    <span className="text-gray-500">{user?.city}, {user?.country}</span>
+                    <span className="text-gray-500 text-xl">{user?.city}, {user?.country}</span>
                 </div>
             </div>
-            <Separator className="w-4/5	m-auto mt-12" />
-            <div className="flex flex-col gap-2 mt-2">
+            <Separator className="w-4/5	m-auto mt-8 mb-8" />
+            <div className="flex flex-col gap-12 mt-2">
                 {user?.personal_info ? <div>
-                    <h6 className="font-medium text-2xl">About Me:</h6>
-                    <p>{user?.personal_info}</p>
+                    <h6 className="font-medium text-4xl text-rose-500">About Me:</h6>
+                    <p className="text-xl">{user?.personal_info}</p>
                 </div> : null}
-                <div><h6 className="font-medium text-2xl">What do I want to give:</h6>
-                    <p>{user?.give}</p>
+                <div><h6 className="font-medium text-4xl text-rose-500">What do I want to give:</h6>
+                    <p className="text-xl">{user?.give}</p>
                 </div>
                 <div>
-                    <h6 className="font-medium text-2xl">What do I want to receive:</h6>
-                    <p>{user?.receive}</p>
+                    <h6 className="font-medium text-4xl text-rose-500">What do I want to receive:</h6>
+                    <p className="text-xl">{user?.receive}</p>
                 </div>
             </div>
+            <Separator className="w-4/5	m-auto mt-8 mb-8" />
             <div className="flex gap-2 m-auto mt-8">
                 <BackButton path={'/timeline'} />
                 {isMyProfile ? <ProfileEditor /> : <Button>Let&apos;s Talk</Button>}
