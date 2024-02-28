97094eb0893557fada4d12a677e5a0cc81c85e18
PeryShahar <pery1shahar@gmail.com>
2024-02-28 16:22:55 +0200

    feat: style of 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+    return <div className="bg-black h-screen font-single flex items-center justify-center ">
+        <div className="flex flex-col items-center">
+            <h1 className="text-rose-500 text-7xl">404</h1>
+            <h2 className="text-5xl">Page Not Found</h2>
+        </div>
+    </div>
+}
+
+export default NotFoundPage
