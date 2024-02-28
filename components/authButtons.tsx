f79002eebe12c5bf687f433ff087a4a23f840682
PeryShahar <pery1shahar@gmail.com>
2024-02-28 12:42:52 +0200

    fix: add handle click for auth btn

--- components/authButtons.tsx
+++ components/authButtons.tsx
@@ -13,6 +13,7 @@ export function GoogleSignInButton() {
 
     return (
         <Button
+            onClick={handleClick}
             borderRadius="1.75rem"
             className=" bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
         >
