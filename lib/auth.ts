917ead0936c486cc505d8601c848cf8da4c319de
PeryShahar <pery1shahar@gmail.com>
2024-03-27 15:28:32 +0200

    fix: cleanup for auth -> remove comments 🧹

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,6 @@
-import { NextAuthOptions, User, getServerSession } from "next-auth";
-import { useSession } from "next-auth/react";
-import { redirect, useRouter } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -22,35 +20,6 @@ export const authConfig: any = {
             clientId: process.env.FACEBOOK_CLIENT_ID as string,
             clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string
         })
-        // CredentialsProvider({
-        //     name: "Sign in",
-        //     credentials: {
-        //         email: {
-        //             label: "Email",
-        //             type: "email",
-        //             placeholder: "example@example.com",
-        //         },
-        //         password: { label: "Password", type: "password" },
-        //     },
-        //     async authorize(credentials) {
-        //         if (!credentials || !credentials.email || !credentials.password)
-        //             return null;
-
-        //         // const dbUser = await prisma.user.findFirst({
-        //         //     where: { email: credentials.email },
-        //         // });
-
-        //         //Verify Password here
-        //         //We are going to use a simple === operator
-        //         //In production DB, passwords should be encrypted using something like bcrypt...
-        //         // if (dbUser && dbUser.password === credentials.password) {
-        //         //     const { password, createdAt, id, ...dbUserWithoutPassword } = dbUser;
-        //         //     return dbUserWithoutPassword as User;
-        //         // }
-
-        //         return null;
-        //     },
-        // }),
     ],
     callbacks: {
         async session({ session, user }: any) {
@@ -64,11 +33,3 @@ export async function loginIsRequiredServer() {
     const session = await getServerSession(authConfig);
     if (!session) return redirect("/");
 }
-
-// export function loginIsRequiredClient() {
-//     if (typeof window !== "undefined") {
-//         const session = useSession();
-//         const router = useRouter();
-//         if (!session) router.push("/");
-//     }
-// }
